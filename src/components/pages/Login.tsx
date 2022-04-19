fd6a47972737672fa964aaa2a410aeee8a57d18c
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-04-19 17:52:25 +0200

    definition for the first pages

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../../hooks/AppContext';
+import { login } from '../../services/user';
 
 export default function Login() : JSX.Element {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const { handleToken, token, navigate } = useAppContext();
+
+  useEffect(() => {
+    if (token !== '' && navigate) {
+      navigate('/');
+    }
+  }, [token, navigate]);
+  const handleSubmit = ():void => {
+    login(username, password, handleToken);
+  };
+
   return (
-    <div>Login</div>
+    <div className="text-center my-5">
+      <h2>Login</h2>
+      <div className="my-4">
+        <input type="text" className="px-4 py-2" placeholder="Enter username" onChange={(event) => { setUsername(event.target.value); }} />
+      </div>
+      <div className="my-4">
+        <input type="password" className="px-4 py-2" placeholder="Enter password" onChange={(event) => { setPassword(event.target.value); }} />
+      </div>
+      <button onClick={handleSubmit} type="button">Submit</button>
+      <div>
+        <Link to="/register">Register</Link>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
   );
 }
--- src/components/pages/Registration.tsx
+++ src/components/pages/Registration.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../../hooks/AppContext';
+import { register } from '../../services/user';
 
 export default function Registration() : JSX.Element {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const { handleToken, handleUser } = useAppContext();
+
+  const handleSubmit = ():void => {
+    register(username, password, handleToken, handleUser);
+  };
+
   return (
-    <div>Registration</div>
+    <div className="text-center my-5">
+      <h2>Registration</h2>
+      <div className="my-4">
+        <input type="text" className="px-4 py-2" placeholder="Enter username" onChange={(event) => { setUsername(event.target.value); }} />
+      </div>
+      <div className="my-4">
+        <input type="password" className="px-4 py-2" placeholder="Enter password" onChange={(event) => { setPassword(event.target.value); }} />
+      </div>
+      <button onClick={handleSubmit} type="button">Submit</button>
+      <div>
+        <Link to="/login">Login</Link>
+        <Link to="/">Home</Link>
+      </div>
+    </div>
   );
 }
