e001051fcb0fdce93ecca635125f36a46bba92d2
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-04-19 17:50:28 +0200

    added new data to provider

--- src/hooks/AppContext.ts
+++ src/hooks/AppContext.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { createContext, useContext } from 'react';
+import { NavigateFunction } from 'react-router-dom';
+import { UserType } from '../utils/types/user';
 
 type AppProviderType = {
     token: string | null,
-    user: string | null
+    user: string,
+    handleToken?: (tokenValue: string) => void,
+    handleUser?: (UserValue: UserType) => void,
+    navigate?: NavigateFunction
 }
 
 export const AppContext = createContext({
-  token: null,
-  user: null,
+  token: '',
+  user: '',
 });
 
 export const useAppContext = (): AppProviderType => useContext(AppContext);
