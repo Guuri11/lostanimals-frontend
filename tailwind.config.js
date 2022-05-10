10aff6e94a5f6a3b62eaebb2bcd966b19ebc9e0f
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-05-10 11:34:37 +0200

    eslint fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
     colors: {
-      'white': '#ffffff',
-      'black': '#000000',
-      'yellow': '#edb506',
-      'dark': '#2d2e2a'
+      white: '#ffffff',
+      black: '#000000',
+      yellow: '#edb506',
+      dark: '#2d2e2a',
     },
   },
   plugins: [],
-}
+};
