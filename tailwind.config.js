e5a53740d011f548609f382242a8d032c0964caf
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-05-11 09:04:45 +0200

    flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
+    './node_modules/flowbite/**/*.js',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
-    colors: {
-      white: '#ffffff',
-      black: '#000000',
-      yellow: '#edb506',
-      dark: '#2d2e2a',
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        yellow: '#edb506',
+        dark: '#2d2e2a',
+        red: '#ff6b6b',
+        green: '#68b96b',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 };
