664a73f39a5fc84f314f44f7e8db782200766be1
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-05-11 09:04:15 +0200

    dummy data for testing

---
+++ src/utils/types/dummy/user.ts
@@ -0,0 +1,12 @@
+import { UserType } from '../user';
+
+export const user: UserType = {
+  '@context': '/contexts/User',
+  '@id': '/users/1',
+  '@type': 'hydra:Item',
+  created_at: '2022-04-17T11:33:08+00:00',
+  updated_at: '2022-04-17T11:33:08+00:00',
+  image: '',
+  posts: [],
+  username: 'dummy_user',
+};
