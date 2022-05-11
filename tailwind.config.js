dc069f40a71c58bd3e33f42e1f4154b730a522c1
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-05-11 10:13:28 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: '#ffffff',
-        black: '#000000',
-        yellow: '#edb506',
+        white: colors.white,
+        blue: colors.blue,
+        black: colors.black,
+        yellow: colors.yellow,
         dark: '#2d2e2a',
         red: '#ff6b6b',
-        green: '#68b96b',
+        green: colors.green,
       },
     },
   },
