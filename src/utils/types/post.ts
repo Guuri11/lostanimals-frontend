9b46e308dae9a83e0829ef0fcd4949c3f970d4cf
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-05-11 09:04:24 +0200

    post & user types

---
+++ src/utils/types/post.ts
@@ -0,0 +1,15 @@
+import { UserType } from './user';
+
+export type PostType = {
+  '@context': string;
+  '@id': string;
+  '@type': string;
+  description: string;
+  type: 'LOST' | 'FOUND';
+  state: boolean;
+  location: string;
+  image: string;
+  owner: UserType;
+  created_at: string;
+  updated_at: string;
+};
