20ec39a2083f2ed4d37c8d241e6660f88af2833d
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-06-03 09:23:08 +0200

    float button modification

--- src/components/elements/FloatButton.tsx
+++ src/components/elements/FloatButton.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { PlusIcon, MenuIcon } from '@heroicons/react/outline';
 
-export default function FloatButton(): JSX.Element {
+type Props = {
+  handleAddPost: () => void
+}
+
+export default function FloatButton({ handleAddPost }: Props): JSX.Element {
   return (
     <div className="floating-container">
       <div className="floating-button">
         <MenuIcon className="p-4">Add Post</MenuIcon>
       </div>
       <div className="element-container">
-        <span className="float-element cursor-pointer">
+        <button type="button" onClick={handleAddPost} className="float-element">
           <PlusIcon className="p-2">Add Post</PlusIcon>
-        </span>
+        </button>
       </div>
     </div>
   );
