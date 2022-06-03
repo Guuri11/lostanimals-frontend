b81619edfedd80d8dacb9ba4ee6c85bea0ccb0ff
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-06-03 09:21:34 +0200

    profile page

--- src/components/pages/AuthPage/Profile/ProfilePresentational.tsx
+++ src/components/pages/AuthPage/Profile/ProfilePresentational.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { PostType } from '../../../../utils/types/post';
+import { UserType } from '../../../../utils/types/user';
+import FloatButton from '../../../elements/FloatButton';
+import Post from '../../../elements/Post/Post';
 
-export default function ProfilePresentational() : JSX.Element {
+type Props = {
+  user: UserType;
+  posts: Array<PostType>;
+  showNoPosts: boolean;
+  handleAddPost: () => void;
+  addPost: boolean;
+}
+
+export default function ProfilePresentational({
+  user, posts, showNoPosts, handleAddPost, addPost,
+}: Props) : JSX.Element {
   return (
-    <>
-      <div>Profile</div>
-      <div>
-        <Link to="/">Go to homepage</Link>
+    <div className="md:flex">
+      <div className="md:w-[33%] md:pr-3 mb-3">
+        { user.image && (
+          <img src={user.image} alt={user.username} />
+        ) }
+        <h1 className="font-bold uppercase">{user.username}</h1>
       </div>
-
-    </>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          { addPost ? <div>hola</div> : null }
+          { posts.map((post) => <Post key={post['@id']} post={post} />) }
+          { showNoPosts && (
+          <div>
+            <h1>No posts near</h1>
+          </div>
+          ) }
+        </div>
+      </div>
+      <FloatButton handleAddPost={handleAddPost} />
+    </div>
   );
 }
