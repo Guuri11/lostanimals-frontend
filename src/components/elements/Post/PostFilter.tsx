06aca1848e5cb37e5df7e353ea8ebb7152770795
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-06-03 09:22:48 +0200

    i dont know actually

--- src/components/elements/Post/PostFilter.tsx
+++ src/components/elements/Post/PostFilter.tsx
@@ -52,11 +52,11 @@ export default function PostFilter({
           <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Type</p>
           <div className="flex">
             <div className="flex items-center mr-4">
-              <input id="type-radio" {...register('type')} type="radio" value="FOUNDED" name="type" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-              <label htmlFor="type-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">FOUNDED</label>
+              <input id="type-radio-filter-found" {...register('type')} type="radio" value="FOUND" name="type" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="type-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">FOUND</label>
             </div>
             <div className="flex items-center mr-4">
-              <input id="type-radio" {...register('type')} type="radio" value="LOST" name="type" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input id="type-radio-filter-lost" {...register('type')} type="radio" value="LOST" name="type" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="type-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">LOST</label>
             </div>
           </div>
