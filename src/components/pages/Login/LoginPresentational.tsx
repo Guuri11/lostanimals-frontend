e607351b53a76b41ea1eb1739107737aef9dafa2
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2022-06-05 15:59:17 +0200

    auth design

--- src/components/pages/Login/LoginPresentational.tsx
+++ src/components/pages/Login/LoginPresentational.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../../elements/Layout';
@@ -7,23 +9,46 @@ type Props = {
   setPassword: React.Dispatch<React.SetStateAction<string>>,
   handleSubmit: () => void
 }
+
 export default function LoginPresentational(
   { setUsername, setPassword, handleSubmit }: Props,
 ) : JSX.Element {
   return (
     <Layout>
-      <div className="text-center my-5">
-        <h2>Login</h2>
-        <div className="my-4">
-          <input type="text" className="px-4 py-2" placeholder="Enter username" onChange={(event) => { setUsername(event.target.value); }} />
-        </div>
-        <div className="my-4">
-          <input type="password" className="px-4 py-2" placeholder="Enter password" onChange={(event) => { setPassword(event.target.value); }} />
-        </div>
-        <button onClick={handleSubmit} type="button">Submit</button>
-        <div>
-          <Link to="/register">Register</Link>
-          <Link to="/">Home</Link>
+      <div className="h-full bg-gradient-to-tl from-green-400 to-yellow-900 w-full">
+        <div className="flex flex-col items-center justify-center">
+          <div className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 mt-16">
+            <p aria-label="Login to your account" className="text-2xl font-extrabold leading-6 text-gray-800">
+              Login to your account
+            </p>
+            <p className="text-sm mt-4 font-medium leading-none text-gray-500">
+              Dont have account?
+              {' '}
+              <Link to="/register" aria-label="Sign up here" className="text-sm font-medium leading-none underline text-gray-800 cursor-pointer">
+                Sign up here
+              </Link>
+            </p>
+            <div className="w-full flex items-center justify-between py-5">
+              <hr className="w-full bg-gray-400" />
+              <p className="text-base font-medium leading-4 px-2.5 text-gray-400">OR</p>
+              <hr className="w-full bg-gray-400  " />
+            </div>
+            <div>
+              <label className="text-sm font-medium leading-none text-gray-800">Email</label>
+              <input onChange={(event) => { setUsername(event.target.value); }} aria-label="enter email adress" type="email" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
+            </div>
+            <div className="mt-6  w-full">
+              <label className="text-sm font-medium leading-none text-gray-800">Password</label>
+              <div className="relative flex items-center justify-center">
+                <input onChange={(event) => { setPassword(event.target.value); }} aria-label="enter Password" type="password" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
+              </div>
+            </div>
+            <div className="mt-8">
+              <button type="button" onClick={handleSubmit} aria-label="create my account" className="focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400 text-sm font-semibold leading-none text-white focus:outline-none bg-yellow-400 border rounded hover:bg-yellow-600 py-4 w-full">
+                Create my account
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
--- src/components/pages/Registration/RegistrationPresentational.tsx
+++ src/components/pages/Registration/RegistrationPresentational.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../../elements/Layout';
@@ -13,18 +14,40 @@ export default function RegistrationPresentational(
 ) : JSX.Element {
   return (
     <Layout>
-      <div className="text-center my-5">
-        <h2>Registration</h2>
-        <div className="my-4">
-          <input type="text" className="px-4 py-2" placeholder="Enter username" onChange={(event) => { setUsername(event.target.value); }} />
-        </div>
-        <div className="my-4">
-          <input type="password" className="px-4 py-2" placeholder="Enter password" onChange={(event) => { setPassword(event.target.value); }} />
-        </div>
-        <button onClick={handleSubmit} type="button">Submit</button>
-        <div>
-          <Link to="/login">Login</Link>
-          <Link to="/">Home</Link>
+      <div className="h-full bg-gradient-to-tl from-green-400 to-yellow-900 w-full">
+        <div className="flex flex-col items-center justify-center">
+          <div className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 mt-16">
+            <p aria-label="Register to your account" className="text-2xl font-extrabold leading-6 text-gray-800">
+              Register to your account
+            </p>
+            <p className="text-sm mt-4 font-medium leading-none text-gray-500">
+              Do you have an account?
+              {' '}
+              <Link to="/login" aria-label="Sign in here" className="text-sm font-medium leading-none underline text-gray-800 cursor-pointer">
+                Sign in here
+              </Link>
+            </p>
+            <div className="w-full flex items-center justify-between py-5">
+              <hr className="w-full bg-gray-400" />
+              <p className="text-base font-medium leading-4 px-2.5 text-gray-400">OR</p>
+              <hr className="w-full bg-gray-400  " />
+            </div>
+            <div>
+              <label className="text-sm font-medium leading-none text-gray-800">Email</label>
+              <input onChange={(event) => { setUsername(event.target.value); }} aria-label="enter email adress" type="email" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
+            </div>
+            <div className="mt-6  w-full">
+              <label className="text-sm font-medium leading-none text-gray-800">Password</label>
+              <div className="relative flex items-center justify-center">
+                <input onChange={(event) => { setPassword(event.target.value); }} aria-label="enter Password" type="password" className="bg-gray-200 border rounded focus:outline-none text-xs font-medium leading-none text-gray-800 py-3 w-full pl-3 mt-2" />
+              </div>
+            </div>
+            <div className="mt-8">
+              <button type="button" onClick={handleSubmit} aria-label="create my account" className="focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400 text-sm font-semibold leading-none text-white focus:outline-none bg-yellow-400 border rounded hover:bg-yellow-600 py-4 w-full">
+                Create my account
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
